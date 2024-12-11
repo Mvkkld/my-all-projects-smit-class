a7cd5fa0114adeaa0222a22715af1c05c59ab387
Mvkkld <153894558+Mvkkld@users.noreply.github.com>
2024-12-12 02:09:31 +0500

    stop watch uploaded

---
+++ STOP WATCH/js/app.js
@@ -0,0 +1,14 @@
+
+var min_html = document.getElementById('min');
+var sec_html = document.getElementById('sec');
+var ms_html = document.getElementById('ms');
+
+var minutes = 0
+var seconds = 0
+var milliSeconds = 0
+
+
+function start(){}
+function stop(){}
+function reset(){}
+
